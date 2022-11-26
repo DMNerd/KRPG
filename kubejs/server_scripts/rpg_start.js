7c9ae0c93c6c2668f075df7e8cf3246cea7ec1e6
Adam <7889445+DMNerd@users.noreply.github.com>
2022-11-26 16:04:19 +0100

    KubeJS

---
+++ kubejs/server_scripts/rpg_start.js
@@ -0,0 +1,8 @@
+// Listen to player login event
+onEvent('player.logged_in', event => {
+  if (!event.player.stages.has('starting_items')) {
+    event.player.stages.add('starting_items')
+    event.player.give('explorerscompass:explorerscompass')
+    event.player.give('naturescompass:naturescompassi')
+  }
+})
