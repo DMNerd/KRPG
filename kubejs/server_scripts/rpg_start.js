dc60cc51cdcdce6a0e0ebe4bcabd50e2dbceb7ed
Adam <7889445+DMNerd@users.noreply.github.com>
2023-03-13 00:54:45 +0100

    Stopped using Kubejs in this pack

--- kubejs/server_scripts/rpg_start.js
+++
@@ -1,8 +0,0 @@
-// Listen to player login event
-onEvent('player.logged_in', event => {
-  if (!event.player.stages.has('starting_items')) {
-    event.player.stages.add('starting_items')
-    event.player.give('explorerscompass:explorerscompass')
-    event.player.give('naturescompass:naturescompassi')
-  }
-})
